3a455e3aefa80f78e680bc93a52638398b529f35
Manuel A. Alonso Tarajano <tarajano@gmail.com>
2017-10-24 20:06:49 -0400

    SUMMARY: Ads radio buttons to select background colors. REMARKS: coursera.org/learn/android-app
    FILE: MainActivity
      ADDS: Methods to change main layout background color.
    
    FILE: main_activity
      ADDS: Ads radio buttons.

--- app/src/main/java/com/example/tarajano/block05/MainActivity.java
+++ app/src/main/java/com/example/tarajano/block05/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.tarajano.block05;
 
+import android.graphics.Color;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+//        RadioButton radioButtonRed = (RadioButton) findViewById(R.id.radioButtonRed);
+//        RadioButton radioButtonBlue = (RadioButton) findViewById(R.id.radioButtonBlue);
+//        RadioButton radioButtonGreen = (RadioButton) findViewById(R.id.radioButtonGreen);
+        final RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+                if(checkedId == R.id.radioButtonBlue){
+                    relativeLayout.setBackgroundColor(Color.BLUE);
+                } else if (checkedId == R.id.radioButtonGreen) {
+                    relativeLayout.setBackgroundColor(Color.GREEN);
+                } else {
+                    relativeLayout.setBackgroundColor(Color.RED);
+                }
+            }
+        });
+
+        relativeLayout.setOnClickListener(new RelativeLayout.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                relativeLayout.setBackgroundColor(Color.WHITE);
+//                RadioButton radioButton = (RadioButton) findViewById ( radioGroup.getCheckedRadioButtonId() );
+//                Log.e("selected button: ", radioButton.toString());
+//                radioButton.setSelected(false);
+            }
+        });
+
     }
 }
